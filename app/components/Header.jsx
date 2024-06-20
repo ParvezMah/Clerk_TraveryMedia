1912573baa811bcd20aa29b71ff86b04d8bb9151
Parvez Mahamud AA <parvezmahmudaa100@gmail.com>
2024-06-20 16:16:33 +0600

    Conditional Header Links completed

--- app/components/Header.jsx
+++ app/components/Header.jsx
@@ -1,10 +1,10 @@
 import Link from 'next/link'
 import React from 'react'
-import { auth } from '@clerk/nextjs'
+import { auth } from "@clerk/nextjs/server"
+import { UserButton } from '@clerk/nextjs';
 
 const Header = () => {
-    const userId = auth();
-    console.log(userId);
+    const { userId } = auth();
   return (
     <>
         <nav className="bg-blue-700 py-4 px-6 flex items-center justify-between mb-5">
@@ -15,9 +15,20 @@ const Header = () => {
                     </div>
                 </Link>
             </div>
-            <div className="text-white">
-                <Link href="/signin" className='text-gray-300 hover:text-white mr-4'>Sign in</Link>
-                <Link href="/signup" className='text-gray-300 hover:text-white mr-4'>Sign up</Link>
+            <div className="text-white flex items-center">
+                {
+                    !userId 
+                        && ( 
+                                <>
+                                    <Link href="/signin" className='text-gray-300 hover:text-white mr-4'>Sign in</Link>
+                                    <Link href="/signup" className='text-gray-300 hover:text-white mr-4'>Sign up</Link>
+                                </>
+                           )
+                }
+                <div className='ml-auto'>
+                    <UserButton/>
+                </div>
+                
             </div>
         </nav>
     </>
--- app/page.jsx
+++ app/page.jsx
@@ -3,7 +3,10 @@
 export default function Home() {
   return (
     <>
-      <h1>Home</h1>
+      <h1 className="text-2xl font-bold mb-5">Welcome</h1>
+      <p className="mb-5">
+        This is a demo site for the Traversy Media Next.js & Clerk tutorial. Go ahead and signin & signup!
+      </p>
     </>
   );
 }
